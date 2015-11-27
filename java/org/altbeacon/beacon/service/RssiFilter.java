0831cf60946ef1a244f86e993cb907e05f93aaa0
jtrfid <jtrfid@qq.com>
2015-11-27 11:09:51 +0800

    添加RssiFilter接口方法注释

--- java/org/altbeacon/beacon/service/RssiFilter.java
+++ java/org/altbeacon/beacon/service/RssiFilter.java
@@ -5,9 +5,11 @@ package org.altbeacon.beacon.service;
  * of RSSI values
  */
 public interface RssiFilter {
-
+    /** 添加测量值 */
     public void addMeasurement(Integer rssi);
+    /** true: 无测量值 */
     public boolean noMeasurementsAvailable();
+    /** 每个扫描周期结束，计算rssi均值或预测值 */
     public double calculateRssi();
 
 }
